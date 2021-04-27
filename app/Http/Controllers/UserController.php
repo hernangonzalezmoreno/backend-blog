0e458bf70a50d30c12562ce97a4bb9e00d8e0aa7
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-04-27 09:43:15 -0300

    UserController@login: Recibiendo los datos en JSON y realizando la validacion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,57 @@ class UserController extends Controller
 
     public function register( Request $request ){
 
+      // Recibir los datos del usuario por post
+      $json = $request->input( 'json', null );// El segundo parametro define en caso que no me llegue el JSON que le asigne null
+      $params = json_decode( $json ); // Decodifica el JSON y lo convierte en un objeto
+      $params_array = json_decode( $json, true ); // Decodifica el JSON y lo convierte en un array
+
+      //var_dump( $json );
+      //var_dump( $params->name );
+      //var_dump( $params_array['name'] );
+
+      // Si estan vacios
+      if( empty( $params ) || empty( $params_array ) ){
+
+        $data = array(
+          'status'  => 'error',
+          'code'    => 404,
+          'message' => 'El JSON está corrupto.',
+        );
+
+        return response()->json( $data, $data['code'] );
+      }
+
+      // Aplico un trim a todos los datos del array para quitarle los espacios
+      $params_array = array_map( 'trim', $params_array );
+
+      // Validar los datos
+      $validated = \Validator::make( $params_array, [
+          'name'      => 'required|alpha',
+          'surname'   => 'required|alpha',
+          'email'     => 'required|email',
+          'password'  => 'required',
+      ]);
+
+      if( $validated->fails() ){
+        //La validacion ha fallado
+        $data = array(
+          'status'  => 'error',
+          'code'    => 404,
+          'message' => 'Datos no validos. El usuario no se a creado.',
+          'errors'  => $validated->errors()
+        );
+
+      }else{
+        //La validacion paso correctamente
+        $data = array(
+          'status'  => 'success',
+          'code'    => 200,
+          'message' => 'El usuario se a creado correctamente.'
+        );
+      }
+
+      return response()->json( $data, $data['code'] );
     }
 
     public function login( Request $request ){
