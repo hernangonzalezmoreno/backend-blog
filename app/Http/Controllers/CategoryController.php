c3d7d281cf6393e477fdb831c46dd61e682dc65f
Hernán González Moreno <hernangonzalezmoreno@gmail.com>
2021-04-30 15:37:52 -0300

    CategoryController@update: metodo para actualizar la categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -94,4 +94,55 @@ class CategoryController extends Controller
       return response()->json( $data, $data[ 'code' ] );
 
     }
+
+    public function update( $id, Request $request ){
+
+      // el metodo input() adminte un segundo parametro para especificar el valor por defecto,
+      // en caso de que no llegue nada. Sin embargo si no llega nada el valor por defecto es null
+      // por lo que no hace falta hacer lo siguiente:
+      // $json = $request->input( 'json', null ); // ya que es redundante
+
+      $json = $request->input('json');
+      $params_array = json_decode( $json, true );
+
+      if( !empty( $params_array ) ){
+
+        // Valido los datos
+        $validate = \Validator::make( $params_array, [
+            'name' => 'required|unique:categories',
+        ]);
+
+        if( !$validate->fails() ){
+
+          // Quito los datos que no quiero actualizar (en caso de que lleguen)
+          unset( $params_array[ 'id' ] );
+          unset( $params_array[ 'created_at' ] );
+
+          // Hago la actualizacion. Si todo sale bien me devuelve 'true'
+          $result = Category::where( 'id', $id )->update( $params_array );
+
+          $data = array(
+            'code'    => 200,
+            'status'  => 'success',
+            'category' => $params_array,
+          );
+
+        }else{
+          $data = array(
+            'code'    => 400,
+            'status'  => 'error',
+            'message' => 'Los parametros no son validos.',
+          );
+        }
+
+      }else{
+        $data = array(
+          'code'    => 400,
+          'status'  => 'error',
+          'message' => 'Faltan los parametros.',
+        );
+      }
+
+      return response()->json( $data, $data['code'] );
+    }
 }
